4ce1ead39ba2ad9ba38ea341031e84c65f965e83
胡鹏 <64hp@163.com>
2018-08-14 19:03:07 +0800

    add

---
+++ NJDYPlayer/Classes/View/NJPlayProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  NJPlayProgressView.swift
+//  NJDYPlayer
+//
+//  Created by HuXuPeng on 2018/8/14.
+//
+
+import UIKit
+
+class NJPlayProgressView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
